68cf84ba2a4ef5498ae522cff69d5382cef5bdfd
Ihza-Fajrur <ihza.119140130@student.itera.ac.id>
2022-04-22 00:45:00 +0700

    .

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import styles from './stylesheet/styles';
+import * as WebBrowser from 'expo-web-browser';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
